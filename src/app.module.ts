d0ac9cd58cbd78410cc63f0ba92f305808fd462c
Elias <eliiasbrondo@gmail.com>
2023-05-17 14:58:59 -0300

    Primer Commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI, { 
+      dbName: process.env.MONGO_DB_NAME
+    }),
+    AuthModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(){
+
+  }
+ }
